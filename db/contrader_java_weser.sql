760166be3241a153fbe483d305a3d9c27d8684f4
paolomascolo <51111877+paolomascolo@users.noreply.github.com>
2019-05-28 17:36:01 +0200

    Update contrader_java_weser.sql

--- db/contrader_java_weser.sql
+++ db/contrader_java_weser.sql
@@ -2,11 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `weser` /*!40100 DEFAULT CHARACTER SET utf8 COLLA
 USE `weser`;
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
-<<<<<<< HEAD
--- Host: localhost    Database: weserusers
-=======
 -- Host: localhost    Database: weser
->>>>>>> b6691aa9e4bad79f3b5f0dd68495122793ad5b56
 -- ------------------------------------------------------
 -- Server version	8.0.15
 
@@ -22,45 +18,10 @@ USE `weser`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
-<<<<<<< HEAD
--- Table structure for table `devices`
---
-
-DROP TABLE IF EXISTS `devices`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `devices` (
-  `dev_id` int(11) NOT NULL AUTO_INCREMENT,
-  `reg_number` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `dev_type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `description` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `min_press` int(4) NULL,
-  `max_press` int(4) NULL,
-  `min_cir` int(4) NULL,
-  `max_cir` int(4) NULL,
-  `min_breath` int(4) NULL,
-  `max_breath` int(4) NULL,
-  `min_temp` float(2,1) NULL,
-  `max_temp` float(2,1) NULL,
-
- PRIMARY KEY (`dev_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Insert all devices';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-
---
--- Table structure for table `users`
---
-
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-=======
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
->>>>>>> b6691aa9e4bad79f3b5f0dd68495122793ad5b56
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -74,21 +35,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Insert all users';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-<<<<<<< HEAD
-
-
---
--- Table structure for table `cares`
---
-
-
-DROP TABLE IF EXISTS `cares`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `cares` (
-  `care_id` int(11) NOT NULL AUTO_INCREMENT,
-  `pat_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-=======
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (1,'admin','admin','weser','user','user','abcdefg');
@@ -129,28 +75,10 @@ DROP TABLE IF EXISTS `cares`;
 CREATE TABLE `cares` (
   `care_id` int(11) NOT NULL AUTO_INCREMENT,
    `pat_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
->>>>>>> b6691aa9e4bad79f3b5f0dd68495122793ad5b56
   `pat_cf` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `pat_age` int(3) COLLATE utf8_unicode_ci NOT NULL,
   `patology` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `care_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-<<<<<<< HEAD
-  `quantity` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `notes` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`care_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Insert all cares';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-
-
---
--- Table structure for table `payments`
---
-
-
-DROP TABLE IF EXISTS `payments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-=======
   `quantity` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `notes` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`care_id`)
@@ -161,7 +89,6 @@ DROP TABLE IF EXISTS `payments`;
 --
 
 DROP TABLE IF EXISTS `payments`;
->>>>>>> b6691aa9e4bad79f3b5f0dd68495122793ad5b56
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `payments` (
   `pay_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -169,44 +96,8 @@ CREATE TABLE `payments` (
   `card_number` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `card_owner` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `card_expire` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-<<<<<<< HEAD
-  `cvv` int(4) COLLATE utf8_unicode_ci NOT NULL, 
-  PRIMARY KEY (`pay_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Insert payments credential';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-
-
-
---
--- Dumping data for table `users`
---
-
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','admin','weser','user','user','abcdfg');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
-
-
-
-
--- Dump completed on 2019-03-19 11:25:37
-=======
   `cvv` int(4) COLLATE utf8_unicode_ci NOT NULL,
  
   PRIMARY KEY (`pay_id`)
   ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Insert payment credentials';
   
->>>>>>> b6691aa9e4bad79f3b5f0dd68495122793ad5b56
